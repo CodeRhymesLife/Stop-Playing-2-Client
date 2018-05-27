302fd11e4fcb5698fb3202735caf74055c6a5510
drryanjames <rcjames1004@gmail.com>
2018-05-27 02:12:13 -0700

    Stop playing fifa after a certain amount of time

--- src/index.js
+++ src/index.js
@@ -2,14 +2,24 @@ import taskkill from 'taskkill'
 
 import Monitor from './monitor'
 
+const Hours = 2
+const Minutes = 0
+const GameTime = 1000 * 60 * (Hours * 60 + Minutes)
+
 const run = () => {
+	const endTime = Date.now() + GameTime
+
 	const monitor = new Monitor({
-		watchList: ['notepad'],
+		watchList: ['FIFA18'],
 
 		// Kill tasks when they show up
-		taskFoundCallback: tasks => tasks.forEach(task => taskkill(task.pid)),
+		taskFoundCallback: tasks => tasks.forEach(task => {
+			const timeLeft = Date.now() - endTime
+			if(timeLeft <= 0)
+				taskkill(task.pid)
+		}),
 
-		interval: 1000,
+		interval: 1000 * 60,
 	})
 	monitor.start()
 }
